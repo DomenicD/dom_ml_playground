79504ea5c37e1190ae516cc28576afcc5188469e
Domenic Donato <info@domenicdonato.com>
2015-09-17 08:01:43 -0700

    Started implementing the SimpleNeuralNetwork.

--- dom_ml_playground/src/experiments/simple_neural_network/simple_neural_network.py
+++ dom_ml_playground/src/experiments/simple_neural_network/simple_neural_network.py
@@ -23,10 +23,38 @@ class NeuralNode(object):
             self.weighedAverage += (self.weights[nodeIndex] *
                                self.incoming[nodeIndex].output)
         
-        self.output = self.activationFn(self.bias.output +
-                                        self.weighedAverage)
+        self.output = self.activationFn(self.bias + self.weighedAverage)
+
+class NetConfig(object):
+    def __init__(self):
+        self.inputCount = 0
+        self.hiddenCount = 0
+        self.outputCount = 0
+        self.inputActivationFn = None # TODO: choose a default
+        self.hiddenActivationFn = None # TODO: choose a default
+        self.outputActivationFn = None # TODO: choose a default
+
+class SimpleNeuralNetwork(object):
+    def __init__(self, config):
+        self.inputLayer = self.createLayer(config.inputCount,
+                                           config.inputActivationFn)
+
+        self.hiddenLayer = self.createLayer(config.hiddenCount,
+                                           config.hiddenActivationFn)
+
+        self.outputLayer = self.createLayer(config.outputCount,
+                                           config.outputActivationFn)
+
+    def createLayer(self, count, activationFn):
+        layer = []
+        for i in range(count):
+            # TODO: Need to have a special input node that has no
+            #       incoming nodes or bias value.
+            layer.append(NeuralNode()) 
+
 
 
 """
+    http://www.iro.umontreal.ca/~bengioy/dlbook/mlp.html#pf2
     f?(x) = b + V sigmoid(c + W x),
 """
